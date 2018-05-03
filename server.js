1b6d5f0b42591f3aada9760ca041a78e5441cd38
daryll <dosis@csumb.edu>
2018-05-03 10:36:06 -0700

    changed the routing

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 var express = require("express");
 var app = express();
 var bodyPasrser = require("body-parser");
+var session = require("express-session");
 
 app.use(bodyPasrser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/static'));
 
+app.use(session({
+    secret: 'denvernuggets',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 70000 }
+}))
 
 app.get('/', function(req, res) {
+    if(!req.session.page_name) { 
+        req.session.name = '';
+    }
+    if(!req.session.location) { 
+        req.session.location = '';
+    }
+    if(!req.session.language) { 
+        req.session.language = '';
+    }
+    if(!req.session.comment) { 
+        req.session.comment = '';
+    }
+    
     res.render('index');
 })
 
 app.post('/process', function(req, res) {
-    res.render('result', {name: req.body.name, location: req.body.location, language: req.body.language, comment: req.body.comment})
+    req.session.name = req.body.name;
+    req.session.location = req.body.location;
+    req.session.language = req.body.language;
+    req.session.comment = req.body.comment;
+
+    res.redirect('/result')
 })
+
+
+app.get('/result', function(req, res) {
+    res.render('result', {name: req.session.name, location: req.session.location, language: req.session.language, comment: req.session.comment});
+})
+
 
 app.listen(8000, function() {
     console.log("server is running");
